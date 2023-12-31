3194a930b6c25dbe5936e0942f958098a9bb4e38
Kibret Zewdu <kbrtbyh@gmail.com>
2023-12-31 11:01:59 +0300

    Linear search implemented

---
+++ Lab01/main.cpp
@@ -0,0 +1,26 @@
+// This program demonstrates the simple searching algorithms
+//Author: Kibret Zewdu
+//Date: December 31, 2023
+
+#include<iostream>
+#include"search.h"
+
+using namespace std;
+
+int main() {
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int searchItem;
+    cout<<"\nEnter an item to search: ";
+    cin>>searchItem;
+
+    int location = lin_search(arr, n, searchItem);
+    if(location == -1){
+        cout<<searchItem<<" is not found!"<<endl;
+    }
+    else {
+        cout<<searchItem<<" is found at location: "<<location<<endl;
+    }
+    return 0;
+
+}
---
+++ Lab01/search.h
@@ -0,0 +1,10 @@
+int lin_search(int arr[], int n, int sKey){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == sKey){
+            return i;
+        }
+    }
+    return -1;
+}
+
+//write an iterative implementation of binary search fiunction
